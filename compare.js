45cf936272abd6b0eae154abca68b5bf21cf4695
htetthuyaaung <htetthuyaaung.15-16@ucsmub.edu.mm>
2020-06-17 10:45:56 +0630

    compare

---
+++ compare.js
@@ -0,0 +1,20 @@
+// var aaMass = 80 ;
+// var aaHeight = 20;
+
+// var mmMass = 70 ;
+// var mmHeight = 25 ;
+
+// var mmbmi =  mmMass / mmHeight * 2;
+// var aabmi = aaMass / aaHeight * 2;
+
+// console.log('Mg Mg BMI is '+ mmbmi);
+// console.log('Aung Aung BMI is '+ aabmi);
+
+// // var who = mmbmi > aabmi;
+// // console.log('Is Mg Mg BMI  higher than Aung Aung?' + who);
+
+// if(mmbmi > aabmi){
+// 	console.log('Mg Mg\s bmi higher than Aung Aung\s bmi');
+// }else{
+// 	console.log('Mg Mg\s bmi smaller than Aung Aung\s bmi');
+// }
