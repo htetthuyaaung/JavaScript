8a2f6e8c958ece132fcfeadd9a1848aef7af0172
htetthuyaaung <htetthuyaaung.15-16@ucsmub.edu.mm>
2020-06-17 10:39:48 +0630

    challenge2

---
+++ challenge2.js
@@ -0,0 +1,17 @@
+// var scoreMgMg = (89 + 120 + 103) / 3;
+// var scoreAungAung = ( 116 + 94 + 123) / 3;
+// var scoreHlaHla = ( 97 + 134 + 105)/ 3;
+// console.log( scoreMgMg);
+// console.log(scoreAungAung);
+// console.log(scoreHlaHla);
+
+// if (scoreMgMg > scoreAungAung && scoreMgMg > scoreHlaHla){
+// 	console.log('Mg Mg team\s win with '+ scoreMgMg);
+// }else if( scoreAungAung > scoreMgMg && scoreAungAung > scoreHlaHla){
+// 	console.log('Aung Aung team\s win with '+ scoreAungAung);
+// }else if(scoreHlaHla > scoreAungAung && scoreHlaHla > scoreMgMg){
+// 	console.log('Hla Hla team\s win with '+ scoreHlaHla);
+// }
+// else{
+// 	console.log('There is a draw');
+// }
