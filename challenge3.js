242b573e24e2374e158ae3a75a858651582e6997
htetthuyaaung <htetthuyaaung.15-16@ucsmub.edu.mm>
2020-06-17 10:41:07 +0630

    challenge3

---
+++ challenge3.js
@@ -0,0 +1,23 @@
+// function tipcal(bill){
+// 	var percentage;
+// 	if(bill<50000){
+// 		percentage = 0.2;
+// 	}else if (bill >= 50000 && bill <100000){
+// 		percentage = 0.15;
+// 	}else{
+// 		percentage = 0.1 ;
+// 	}
+
+// 	return percentage*bill;
+// }
+
+
+// console.log(tipcal(35000));/ console.log(tipcal(72000));
+// console.log(tipcal(120000));
+
+// var bill = [35000, 72000, 120000];
+// var tip = [tipcal(bill[0]), tipcal(bill[1]), tipcal(bill[2])];
+// console.log(tip);
+
+// var tot = [bill[0] + tip[0] , bill[1] + tip[1] ,bill[2] + tip[2]];
+// console.log(tot);
